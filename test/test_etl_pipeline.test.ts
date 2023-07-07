b7dcd86d28e05542714ff20dae396f700ec2581a
zqumei <zaid.qumei+os@gmail.com>
2023-07-06 20:16:03 -0400

    Initial commit

---
+++ test/test_etl_pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestEtlPipeline from '../lib/test_etl_pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/test_etl_pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestEtlPipeline.TestEtlPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
